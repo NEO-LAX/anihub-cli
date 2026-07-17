29b621930016da8a8889acd196a79f1fbfbca8c5
NEOLAX <neolax@proton.me>
2026-07-17 22:03:03 +0300

    chore: set release version to 0.7.1
    Package/flake version matches the clean v0.7.1 release tag and includes
    the franchise season-sources fix.

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
       anihub-cli = pkgs.rustPlatform.buildRustPackage {
         pname = "anihub-cli";
-        version = "0.7.0";
+        version = "0.7.1";
         src = pkgs.lib.cleanSource ./.;
         cargoLock.lockFile = ./Cargo.lock;
 
